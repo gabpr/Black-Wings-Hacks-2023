7b4ba1a3121e3ddc89cde0d491ae027ea1c29b18
Gabriela Perez <gabrielaperezgil@utexas.edu>
2023-02-04 13:39:42 -0600

    added Form.js for login page

---
+++ app/src/Form.js
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+export default function Form() {
+
+// States for registration
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
+
+// States for checking the errors
+const [submitted, setSubmitted] = useState(false);
+const [error, setError] = useState(false);
+
+// Handling the email change
+const handleUsername = (e) => {
+setUsername(e.target.value);
+setSubmitted(false);
+};
+
+// Handling the password change
+const handlePassword = (e) => {
+setPassword(e.target.value);
+setSubmitted(false);
+};
+
+// Handling the form submission
+const handleSubmit = (e) => {
+e.preventDefault();
+if (username === '' || password === '') {
+setError(true);
+} else {
+setSubmitted(true);
+setError(false);
+}
+};
+
+// Showing success message
+const successMessage = () => {
+return (
+<div
+className="success"
+style={{
+display: submitted ? '' : 'none',
+}}>
+<h1>User {username}  successfully logged in!</h1>
+</div>
+);
+};
+
+// Showing error message if error is true
+const errorMessage = () => {
+return (
+<div
+className="error"
+style={{
+display: error ? '' : 'none',
+}}>
+<h1>Please enter all the fields</h1>
+</div>
+);
+};
+
+return (
+<div className="form">
+<div>
+<h1>Log in</h1>
+</div>
+
+{/* Calling to the methods */}
+<div className="messages">
+{errorMessage()}
+{successMessage()}
+</div>
+
+<form>
+{/* Labels and inputs for form data */}
+
+<label className="label">username</label>
+<input onChange={handleUsername} className="input"
+value={username} type="email" />
+
+<label className="label">password</label>
+<input onChange={handlePassword} className="input"
+value={password} type="password" />
+
+<button onClick={handleSubmit} className="btn" type="submit">
+Submit
+</button>
+</form>
+</div>
+);
+}
